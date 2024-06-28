eaef6a80663ad94e62582f2d72ee1c46792ead8b
LuuizPaes <lu.fernando.paes@hotmail.com>
2024-06-28 11:48:09 -0300

    Inserindo as funções no javascript, estilizando o bg e tambem os elementos

--- products.js
+++ products.js
@@ -1,15 +1,15 @@
 const menuOptions = [
 
-    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.png' },
+    { name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.jpeg' },
     
-    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './assets/xbacon.png' },
     
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/bacon-egg.png' },
     
-    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
+    { name: 'Monstruoso', price: 50, vegan: false, src: './assets/monstruoso.png' },
     
-    { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
+    { name: 'Big Vegano', price: 55, vegan: true, src: './assets/xvegan.png' },
     
-    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
+    { name: 'X-Vegan', price: 45, vegan: true, src: './assets/monstruoso-vegan.png' },
     
     ]
--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-console.log(menuOptions)
+const list = document.querySelector('ul')
+const showItems = document.querySelector('.show-items')
+const mapItems = document.querySelector('.map-items')
+const sumItems = document.querySelector('.sum-items')
+const filterItems = document.querySelector('.filter-items')
+
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    return newValue
+}
+
+
+function showAllItems(productsArray) {
+    let myLi = ''
+    productsArray.forEach( product => {
+        
+        
+        myLi += 
+        `
+        <li>
+        <img src= ${product.src}>
+        <p> ${product.name} </p>
+        <p class="item-price"> ${formatCurrency(product.price)} </p>
+        </li>
+        `
+        
+    })
+    
+    list.innerHTML = myLi
+}
+
+function mapAllItems() {
+    const newPrice = menuOptions.map( (product) => ({
+        ...product, //spread operator
+        price: product.price * 0.9, //10% de desconto
+        
+    }))
+
+    showAllItems(newPrice)
+
+}
+
+function sumAllItems() {
+    const sumAll = menuOptions.reduce((acc, value) => {
+        return  acc + value.price 
+        
+    }, 0)
+
+    list.innerHTML =
+    `
+    <li>
+        <p> O valor total dos produtos é R$ ${formatCurrency(sumAll)} </p>
+    </li>
+    
+    `
+}
+
+function filterAllItems() {
+    const veganItems = menuOptions.filter( (product) => product.vegan)
+   
+
+    showAllItems(veganItems)
+}
+
+
+
+
+showItems.addEventListener('click',  () => showAllItems(menuOptions)) //função vazia para chamar a função assim que o botão for clicado.
+mapItems.addEventListener('click', mapAllItems)
+sumItems.addEventListener('click', sumAllItems)
+filterItems.addEventListener('click', filterAllItems)
